8f7f1cb470ebb433548c0c0a3b509e950d6c06de
MaryLensin <88140591+MaryLensin@users.noreply.github.com>
2021-10-10 11:11:44 +0300

    Add files via upload

--- Application.java
+++ Application.java
@@ -9,23 +9,23 @@ public class Application {
 	public static void main(String[] args) {
 		Box firstBox = new Box();
 		Box secondBox = new Box();
-		
-		Book book = new Book(1157,"Uglies","Scott Westerfild",2005);
-		Journal journal = new Journal(157,"Vogue","Fashion");
-		
+
+		Book book = new Book(1157, "Uglies", "Scott Westerfild", 2005);
+		Journal journal = new Journal(157, "Vogue", "Fashion");
+
 		firstBox.setContent(book);
 		secondBox.setContent(journal);
-		
+
 		System.out.println(firstBox.toString());
 		System.out.println(secondBox.toString());
-		
+		// одну и ту же книгу книгу в обе коробки конечно положить можно
 		secondBox.setContent(book);
 		book.setTitle("Extra");
-		
+		// если мы меняем название книги в первой коробке, то меняется и название в
+		// другой, ведь это буквально один и тот же объект, одно и то же место в памяти,
+		// на который ссылаются обе коробки
 		System.out.println(firstBox.toString());
 		System.out.println(secondBox.toString());
-		
-		
 
 	}
 
